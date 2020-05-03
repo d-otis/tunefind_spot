0667146460730745400471c44ed8f937f142c6b0
d-otis <dan@dan-foley.com>
2020-05-03 13:02:51 -0400

    add explanatory commenting

--- lib/spotify.rb
+++ lib/spotify.rb
@@ -1,3 +1,5 @@
+# THIS FILE IS JUST FOR DEBUGGING DURING DEV
+
 CONFIG = {
   :access_token => "BQB6kaCw3--HAhG62lEIgPkD5JcFqhuHM2KT7ZzjwNjPyq1IZw0kxaCqzce73kb11P39Q4sMscEFUG52Z9pAS4Ub-z0Ff-YpBEanP-fAJVugNK8mmsH31lfX2Hams2LSAFcG2sA5gI53B4-_judcMA3MYLkySiWkyhaURmy4TValsTtchrR3riAInsXxBF9Ut_wJDvH8ZqyFE6nzP1tNxVEw7GJTvxbl8qFQtM404MrRL15DEAabTZF5D65Hq2sqBSvzrFCZE5DFvPuX",
   :raise_errors => true,  # choose between returning false or raising a proper exception when API calls fails
